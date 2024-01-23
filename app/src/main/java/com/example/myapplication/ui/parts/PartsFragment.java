b50a039e6ee4ee84101371df2c28b8e31a47dca7
imzbb <39257559+imzbb@users.noreply.github.com>
2024-01-23 15:41:49 +0800

    1.0.2 配件库新增按钮

--- app/src/main/java/com/example/myapplication/ui/parts/PartsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/parts/PartsFragment.java
@@ -1,16 +1,21 @@
 package com.example.myapplication.ui.parts;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentPartsBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PartsFragment extends Fragment {
 
@@ -22,11 +27,33 @@ public class PartsFragment extends Fragment {
                 new ViewModelProvider(this).get(PartsViewModel.class);
 
         binding = FragmentPartsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = binding.getRoot();
 
         final TextView textView = binding.textParts;
         partsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+        FloatingActionButton floatingActionButton = view.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("提示")
+                        .setMessage("这是一个提示框")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // 在这里编写点击确定按钮后的逻辑
+                            }
+                        })
+                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // 在这里编写点击取消按钮后的逻辑
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+        return view;
     }
 
     @Override
